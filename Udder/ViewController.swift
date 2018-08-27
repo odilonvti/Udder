6e86bb2b22b800e34a3c4903883809a2f512ba02
Odilon Pereira <novomac@macbook-pro-de-odilon.local>
2018-08-26 21:33:08 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Odilon Pereira on 8/26/18.
+//  Copyright © 2018 odilon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
